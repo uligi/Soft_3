7d4ea6eec96768c8e12500865af6a82cde0acbbe
uligi <59987716+uligi@users.noreply.github.com>
2024-06-30 17:14:47 -0600

    sdfgsrg

--- DUNAMIS_SA/Controllers/CargaController.cs
+++ DUNAMIS_SA/Controllers/CargaController.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using DUNAMIS_SA.Data;
 using DUNAMIS_SA.Models;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DUNAMIS_SA.Controllers
 {
@@ -18,9 +18,9 @@ namespace DUNAMIS_SA.Controllers
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var cargas = _context.Cargas.FromSqlRaw("EXEC ObtenerCargas").ToList();
+            var cargas = await _context.Cargas.Include(c => c.TipoDeCarga).Include(c => c.Cliente).ToListAsync();
             return View(cargas);
         }
 
@@ -31,30 +31,26 @@ namespace DUNAMIS_SA.Controllers
             {
                 if (carga.CargasID == 0)
                 {
-                    var parameters = new[]
-                    {
+                    await _context.Database.ExecuteSqlRawAsync(
+                        "EXEC CrearCarga @Peso, @FechaEnvio, @Destino, @TipoDeCargaID, @ClienteID",
                         new SqlParameter("@Peso", carga.Peso),
                         new SqlParameter("@FechaEnvio", carga.FechaEnvio),
                         new SqlParameter("@Destino", carga.Destino),
                         new SqlParameter("@TipoDeCargaID", carga.TipoDeCargaID),
                         new SqlParameter("@ClienteID", carga.ClienteID)
-                    };
-
-                    await _context.Database.ExecuteSqlRawAsync("EXEC CrearCarga @Peso, @FechaEnvio, @Destino, @TipoDeCargaID, @ClienteID", parameters);
+                    );
                 }
                 else
                 {
-                    var parameters = new[]
-                    {
-                        new SqlParameter("@CargasID", carga.CargasID),
+                    await _context.Database.ExecuteSqlRawAsync(
+                        "EXEC ActualizarCarga @CargaID, @Peso, @FechaEnvio, @Destino, @TipoDeCargaID, @ClienteID",
+                        new SqlParameter("@CargaID", carga.CargasID),
                         new SqlParameter("@Peso", carga.Peso),
                         new SqlParameter("@FechaEnvio", carga.FechaEnvio),
                         new SqlParameter("@Destino", carga.Destino),
                         new SqlParameter("@TipoDeCargaID", carga.TipoDeCargaID),
                         new SqlParameter("@ClienteID", carga.ClienteID)
-                    };
-
-                    await _context.Database.ExecuteSqlRawAsync("EXEC ActualizarCarga @CargasID, @Peso, @FechaEnvio, @Destino, @TipoDeCargaID, @ClienteID", parameters);
+                    );
                 }
                 return Json(new { success = true });
             }
@@ -64,14 +60,8 @@ namespace DUNAMIS_SA.Controllers
         [HttpPost]
         public async Task<IActionResult> Delete(int id)
         {
-            var parameter = new SqlParameter("@CargasID", id);
-            var result = await _context.Database.ExecuteSqlRawAsync("EXEC EliminarCarga @CargasID", parameter);
-
-            if (result > 0)
-            {
-                return Json(new { success = true });
-            }
-            return Json(new { success = false, message = "Carga not found." });
+            await _context.Database.ExecuteSqlRawAsync("EXEC EliminarCarga @CargaID", new SqlParameter("@CargaID", id));
+            return Json(new { success = true });
         }
     }
 }
