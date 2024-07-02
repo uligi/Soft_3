69e10a4bbe967f3f73723507428db49c0cd27ab8
uligi <59987716+uligi@users.noreply.github.com>
2024-07-02 09:39:30 -0600

    f

--- DUNAMIS_SA/obj/Debug/net8.0/DUNAMIS_SA.AssemblyInfo.cs
+++ DUNAMIS_SA/obj/Debug/net8.0/DUNAMIS_SA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DUNAMIS_SA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc9454fd6afcd521a7fa0b3f45aa1816010ca9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f78690ade1ce16675c1589146a445f23545882f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DUNAMIS_SA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DUNAMIS_SA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
